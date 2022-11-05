5bcea1c46ee7364f10e3cc9833e57be9e8796852
Leulseged <leulbekele191@gmail.com>
2022-11-05 22:37:10 +0300

    csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module to export data in the CSV format"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    id_no = argv[1]
+    todo = requests.get(url + 'todos?userId={}'.format(id_no)).json()
+    user = requests.get(url + 'users/{}'.format(id_no)).json()
+    with open(str(id_no) + '.csv', 'w', newline='') as fi:
+        writer = csv.writer(fi, quoting=csv.QUOTE_ALL)
+        for to in todo:
+            writer.writerow([user.get('id'), user.get('username'),
+                             to.get('completed'), to.get('title')])
